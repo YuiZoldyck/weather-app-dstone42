a75d7da55b6728ed4954e2d8bb3c37d271ebfca7
David Stone <david.gstone42@gmail.com>
2022-02-11 23:10:07 -0700

    Added CSS

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ document.getElementById("weatherSubmit").addEventListener("click", function(even
         }).then(function(json) {
             console.log(json);
             let results = "";
-            results += '<h2>Weather in ' + json.name + "</h2>";
+            results += '<h2 class="header">Weather in ' + json.name + "</h2>";
             for (let i=0; i < json.weather.length; i++) {
                 results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
             }
-            results += '<h2>Temperature: ' + json.main.temp + " &deg;F</h2>"
-            results += '<h2>Feels Like: ' + json.main.feels_like + " &deg;F</h2>"
-            results += '<h2>Wind Speed: ' + json.wind.speed + " Meter/s</h2>"
+            results += '<h3 class="header">Temperature: ' + json.main.temp + " &deg;F</h3>"
+            results += '<h3 class="header">Feels Like: ' + json.main.feels_like + " &deg;F</h3>"
+            results += '<h3 class="header">Wind Speed: ' + json.wind.speed + " Meter/s</h3>"
             results += "<p>"
             for (let i=0; i < json.weather.length; i++) {
                 results += json.weather[i].description
@@ -36,12 +36,16 @@ document.getElementById("weatherSubmit").addEventListener("click", function(even
       }).then(function(json) {
         console.log(json);
         let forecast = "";
-        forecast += "<h2>Forecast for " + json.city.name + "</h2>"
+        forecast += "<h2 class='header'>Forecast for " + json.city.name + "</h2>"
+        forecast += "<div id='forecast-container'>"
         for (let i=0; i < json.list.length; i++) {
-            forecast += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
+            forecast += "<div class='forecast'>"
+            forecast += "<h3 class='header'>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h3>";
             forecast += "<p>Temperature: " + json.list[i].main.temp + "</p>";
             forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+            forecast += "</div>"
         }
+        forecast += "</div>"
         document.getElementById("forecastResults").innerHTML = forecast;
       });
 });
